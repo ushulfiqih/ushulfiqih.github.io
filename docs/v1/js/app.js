b055e5c8aba6a2e5bb4491a700496e9e55beb0fa
@faridiot <dev@designer.web.id>
2021-01-10 19:40:15 +0700

    v1

---
+++ docs/v1/js/app.js
@@ -0,0 +1,76 @@
+// Dom7
+var $ = Dom7;
+
+// Theme
+var theme = 'md';
+if (document.location.search.indexOf('theme=') >= 0) {
+  theme = document.location.search.split('theme=')[1].split('&')[0];
+}
+
+// Init App
+var app = new Framework7({
+  id: 'io.framework7.testapp',
+  root: '#app',
+  theme: theme,
+	view: {
+		pushState :true,
+		stackPages: true,
+	},
+  // Extended by Dialog component:
+   dialog: {
+     title: 'Ushulfiqih.com',
+   },
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+    };
+  },
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  routes: routes,
+  popup: {
+    closeOnEscape: true,
+  },
+  sheet: {
+    closeOnEscape: true,
+  },
+  popover: {
+    closeOnEscape: true,
+  },
+  actions: {
+    closeOnEscape: true,
+  },
+  vi: {
+    placementId: 'pltd4o7ibb9rc653x14',
+  },
+});
+
+
+
+
+
+
+
+// Create Popup with swipe to close
+var swipeToClosePopup = app.popup.create({
+  el: '.sample',
+  swipeToClose: true,
+});
+
+// Create Popup with swipe to close
+var swipeToClosePopup = app.popup.create({
+  el: '.daftar',
+  swipeToClose: true,
+});
+
+// Create Popup with swipe to close
+var swipeToClosePopup = app.popup.create({
+  el: '.lupapwd',
+  swipeToClose: true,
+});
---
+++ docs/v1/js/routes.js
@@ -0,0 +1,117 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+    name: 'home',
+  },
+  {
+    path: '/tab-search/',
+    url: './pages/tab-search.html',
+    name: 'home',
+  },
+  {
+    path: '/tab-search-input/',
+    url: './pages/tab-search-input.html',
+    name: 'home',
+  },
+  {
+    path: '/tab-forum/',
+    url: './pages/tab-forum.html',
+    name: 'home',
+  },
+  {
+    path: '/tab-forum-discuss/',
+    url: './pages/tab-forum-discuss.html',
+    name: 'home',
+  },
+  {
+    path: '/tab-forum-akun/',
+    url: './pages/tab-forum-akun.html',
+    name: 'home',
+  },
+{
+    path: '/tab-forum-login/',
+    url: './pages/tab-forum-login.html',
+    name: 'home',
+  },
+{
+    path: '/tab-forum-register/',
+    url: './pages/tab-forum-register.html',
+    name: 'home',
+  },
+{
+    path: '/tab-forum-reset/',
+    url: './pages/tab-forum-reset.html',
+    name: 'home',
+  },
+{
+    path: '/tab-forum-out/',
+    url: './pages/tab-forum-out.html',
+    name: 'home',
+  },
+{
+    path: '/tab-forum-dashboard/',
+    url: './pages/tab-forum-dashboard.html',
+    name: 'home',
+  },
+  // Components
+ {
+    path: '/tab-about/',
+    componentUrl: './pages/tab-about.html',
+  },
+  {
+     path: '/tab-kaidah/',
+     componentUrl: './pages/tab-kaidah.html',
+   },
+   {
+      path: '/dialog/',
+      componentUrl: './pages/dialog.html',
+    },
+   {
+      path: '/tab-home/',
+      componentUrl: './pages/tab-home.html',
+    },
+  {
+    path: '/pengertian-ijtihad/',
+    componentUrl: './teks/pengertian-ijtihad.html',
+  },
+  {
+    path: '/dasar-hukum-ijtihad/',
+    componentUrl: './teks/dasar-hukum-ijtihad.html',
+  },
+  {
+    path: '/macam-macam-ijtihad/',
+    componentUrl: './teks/macam-macam-ijtihad.html',
+  },
+  {
+    path: '/syarat-ijtihad/',
+    componentUrl: './teks/syarat-ijtihad.html',
+  },
+  {
+    path: '/objek-ijtihad/',
+    componentUrl: './teks/objek-ijtihad.html',
+  },
+  {
+    path: '/hukum-melakukan-ijtihad/',
+    componentUrl: './teks/hukum-melakukan-ijtihad.html',
+  },
+  {
+    path: '/tingkatan-mujtahid/',
+    componentUrl: './teks/tingkatan-mujtahid.html',
+  },
+  {
+    path: '/pengertian-istihsan/',
+    componentUrl: './teks/pengertian-istihsan.html',
+  },
+  // Privacy
+  {
+    path: '/privacy/',
+    componentUrl: './pages/privacy.html',
+  },
+  // Default route (404 page). MUST BE THE LAST
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+];
