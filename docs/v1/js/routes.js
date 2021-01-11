7bb2884df2ca5d96b490101494d38e5c86a50b7a
@faridiot <dev@designer.web.id>
2021-01-11 21:15:19 +0700

    fix search

--- docs/v1/js/routes.js
+++ docs/v1/js/routes.js
@@ -11,11 +11,6 @@ var routes = [
     name: 'home',
   },
   {
-    path: '/tab-search-input/',
-    url: './pages/tab-search-input.html',
-    name: 'home',
-  },
-  {
     path: '/tab-forum/',
     url: './pages/tab-forum.html',
     name: 'home',
