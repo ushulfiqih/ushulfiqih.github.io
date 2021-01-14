2c087b10fda511080f9d27e873f4109afce65f69
@faridiot <dev@designer.web.id>
2021-01-14 20:50:38 +0700

    xl style

--- docs/free/js/custom.js
+++ docs/free/js/custom.js
@@ -4,4 +4,8 @@ setTimeout(function() {
 
 setTimeout(function() {
 	$('#mytitle').fadeOut('fast');
-}, 20000); // <-- time in milliseconds
+}, 8000); // <-- time in milliseconds
+
+setTimeout(function() {
+	$('#mytitle2').fadeOut('fast');
+}, 8000); // <-- time in milliseconds
