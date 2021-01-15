3b50585139e525c891b219d04c04f6b465be4a73
@faridiot <dev@designer.web.id>
2021-01-15 10:57:40 +0700

    link external

--- docs/free/js/custom.js
+++ docs/free/js/custom.js
@@ -9,3 +9,21 @@ setTimeout(function() {
 setTimeout(function() {
 	$('#mytitle2').fadeOut('fast');
 }, 8000); // <-- time in milliseconds
+
+
+
+
+return {
+	on: {
+		pageInit: function () {
+			var self = this;
+			self.sheetSwipeToStep = self.$app.sheet.create({
+				el: '.demo-sheet-swipe-to-step',
+				swipeToClose: true,
+				swipeToStep: true,
+				push: true,
+				backdrop: true,
+			});
+		},
+	}
+}
