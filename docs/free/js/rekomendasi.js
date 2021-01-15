41ec229e6c915a40aebda7e03417063ed564f832
@faridiot <dev@designer.web.id>
2021-01-15 09:17:05 +0700

    home content

--- docs/free/js/rekomendasi.js
+++ docs/free/js/rekomendasi.js
@@ -17,3 +17,13 @@ $(elems[i]).hide();
 }
 }
 }
+
+var elems = $("ushulfiqihcom");
+if (elems.length) {
+var keep = Math.floor(Math.random() * elems.length);
+for (var i = 0; i < elems.length; ++i) {
+if (i !== keep) {
+$(elems[i]).hide();
+}
+}
+}
